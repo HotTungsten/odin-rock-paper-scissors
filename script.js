dc9d97b39cb76ffba89602b29295e8444818c0c8
unknown <hottungsten@gmail.com>
2022-04-16 22:57:22 -0700

    allows multiple rounds

--- script.js
+++ script.js
@@ -14,33 +14,46 @@ function computerPlay() {
 }
 
 function playRound(computerChoice, userChoice) {
+    computerChoice = computerPlay();
+    userChoice = prompt("Enter rock, paper, or scissors");
+
+    //scrub user input
+    userChoice = userChoice.toLowerCase();
+
     if(userChoice === computerChoice){
         return "Draw!";
     }
     else if(userChoice === "scissors" && computerChoice === "rock"){
-        return "Computer wins!";
+        return "Computer wins! rock beats scissors";
     }
     else if(userChoice === "scissors" && computerChoice === "paper"){
-        return "User wins!"
+        return "User wins! scissors beats paper";
     }
     else if(userChoice === "paper" && computerChoice === "rock"){
-        return "User wins!"
+        return "User wins! paper beats rock";
     }
     else if(userChoice === "paper" && computerChoice === "scissors"){
-        return "Computer wins!"
+        return "Computer wins! scissors beats paper";
     }
     else if(userChoice === "rock" && computerChoice === "paper"){
-        return "Computer wins!"
+        return "Computer wins! rock beats paper";
     }
     else if(userChoice === "rock" && computerChoice === "scissors"){
-        return "User wins!"
+        return "User wins! rock beats scissors";
     }
 }
 
-let computerChoice = computerPlay();
-let userChoice = prompt("Enter rock, paper, or scissors");
+function game(){
+    for(let i = 0; i < 5; i++){
+        playRound();
+    }
+}
 
-//scrub user input
-userChoice = userChoice.toLowerCase();
+game();
+// let computerChoice = computerPlay();
+// let userChoice = prompt("Enter rock, paper, or scissors");
 
-console.log(playRound(computerChoice, userChoice));
+// //scrub user input
+// userChoice = userChoice.toLowerCase();
+
+// console.log(playRound(computerChoice, userChoice));
