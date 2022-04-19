edbab7499e5105d48ab7e1ecf1365dbd1b59b40c
unknown <hottungsten@gmail.com>
2022-04-19 10:57:59 -0700

    button functionality finished

--- script.js
+++ script.js
@@ -13,13 +13,11 @@ function computerPlay() {
     }
 }
 
-function playRound(computerChoice, userChoice) {
-    computerChoice = computerPlay();
-    userChoice = prompt("Enter rock, paper, or scissors");
+function playRound(userChoice) {
+    let computerChoice = computerPlay();
 
-    //scrub user input
-    userChoice = userChoice.toLowerCase();
-
+    const resultDiv = document.querySelector(".results");
+    
     if(userChoice === computerChoice){
         return "Draw!";
     }
@@ -43,18 +41,51 @@ function playRound(computerChoice, userChoice) {
     }
 }
 
-const rockButton = document.querySelector(".rock");
-const paperButton = document.querySelector(".paper");
-const scissorsButton = document.querySelector(".scissors");
+function game() {
+    const rockButton = document.querySelector(".rock");
+    const paperButton = document.querySelector(".paper");
+    const scissorsButton = document.querySelector(".scissors");
 
-const resultDiv = document.querySelector(".results");
+    let resultDiv = document.querySelector(".results");
+    let scoreDiv = document.querySelector(".currScore");
 
-rockButton.addEventListener('click', () => {
-    resultDiv.textContent = "rock";
-});
-paperButton.addEventListener('click', () => {
-    resultDiv.textContent = "paper";
-});
-scissorsButton.addEventListener('click', () => {
-    resultDiv.textContent = "scissors";
-});
+    let results;
+    let userScore = 0;
+    let computerScore = 0;
+    
+    rockButton.addEventListener('click', () => {
+        results = playRound("rock");
+        resultDiv.textContent = results;
+        if(results.includes("User")){
+            userScore++;
+        }
+        else if(results.includes("Computer")){
+            computerScore++;
+        }
+        scoreDiv.textContent = "Player Score: " + userScore + " Computer Score: " + computerScore;
+    });
+    paperButton.addEventListener('click', () => {
+        results = playRound("paper");
+        resultDiv.textContent = results;
+        if(results.includes("User")){
+            userScore++;
+        }
+        else if(results.includes("Computer")){
+            computerScore++;
+        }
+        scoreDiv.textContent = "Player Score: " + userScore + " Computer Score: " + computerScore;
+    });
+    scissorsButton.addEventListener('click', () => {
+        results = playRound("scissors");
+        resultDiv.textContent = results;
+        if(results.includes("User")){
+            userScore++;
+        }
+        else if(results.includes("Computer")){
+            computerScore++;
+        }
+        scoreDiv.textContent = "Player Score: " + userScore + " Computer Score: " + computerScore;
+    });
+}
+
+game();
